fdf72c3e248577a1964a4e6278066017df559dd2
Alice <alice@Alices-MacBook-Pro.local>
2017-12-05 16:29:11 -0500

    added CORS and some of the beginnings of web server

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-@app.route('/')
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/test')
 def hello_world():
     return 'Hello, world2'
+
+@app.route('/', methods=['POST'])
+def validate():
+        img = request.get_json()
+        img_string = img['image']
+        str = "received"
+        return str

808cf2726421953c5f4bb55052ec51b0b6e8a1df
Alice <alice@Alices-MacBook-Pro.local>
2017-12-05 21:47:56 -0500

    save image to set path

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from time import gmtime, strftime
+import base64
+import re
 
 app = Flask(__name__)
 CORS(app)
@@ -12,5 +15,11 @@ def hello_world():
 def validate():
         img = request.get_json()
         img_string = img['image']
+	filename = strftime("%Y%m%d_%H_%M_%S", gmtime())
+	PATH = "images/"
+	file_ending = ".jpg"
+	filepath = PATH + filename + file_ending
+	with open (filepath, "w") as image_out:
+		image_out.write(re.sub('^data:image/.+;base64,', '', img_string).decode('base64'))
         str = "received"
         return str

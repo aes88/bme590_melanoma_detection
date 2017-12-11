c9e2d6182cabbb939c82dd804e5e63b74556fa56
Alice <alice@Alices-MacBook-Pro.local>
2017-12-11 09:46:20 -0500

    got database to save

--- main.py
+++ main.py
@@ -19,12 +19,24 @@ def validate():
         img_string = img['image']
 	filename = strftime("%Y%m%d_%H_%M_%S", gmtime())
 	PATH = "images/"
-	file_ending = ".jpg"
+	check_jpg = re.search("^data:image/jpeg",img_string)
+	if (check_jpg != None):
+		file_ending = ".jpg"
+	else:
+		check_png = re.search("^data:image/png", img_string)
+		if (check_png != None):
+			file_ending = ".png"
+		else:
+			check_gif = re.search("^data:image/gif", img_string)
+			if (check_gif != None):
+				file_ending = ".gif"
+			else:
+				error_string = "Error: Incorrect file type uploaded"
+				return error_string
 	filepath = PATH + filename + file_ending
 	with open (filepath, "w") as image_out:
 		image_out.write(re.sub('^data:image/.+;base64,', '', img_string).decode('base64'))
         labels, predictions = predict(filepath)
-	#str = "received"
 	name = "test"
 	malignant = 0
 	benign = 0
@@ -38,4 +50,3 @@ def validate():
 	results = {"labels": labels2, "probabilities": predict3}
 	write_file(name,file_ending,filepath,malignant,benign,symmetry,border,color,diameter)
         return str(results)
-	#return str
